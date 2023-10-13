cdd4b7787333085f6d3e135f92b07d0f6e66b105
memoTux <romeo@mendezfuentes.net>
2023-10-13 16:21:57 -0600

    [h3] add handleCors

--- h3/index.js
+++ h3/index.js
@@ -1,22 +1,29 @@
 import { createServer } from 'node:http'
 import { WebSocketServer } from 'ws'
-import { createApp, eventHandler, toNodeListener, readBody } from 'h3'
+import { createApp, eventHandler, toNodeListener, readBody, handleCors } from 'h3'
 import { useServer } from 'graphql-ws/lib/use/ws'
 import { graphql } from 'graphql'
 import schema from './schema.js'
 
 const app = createApp()
-app.use(
-  '/graphql',
-  eventHandler(async (event) => {
-    const body = await readBody(event)
-
-    return graphql({
-      schema,
-      source: body.query,
+app
+  .use(
+    '/graphql',
+    eventHandler((event) => {
+      handleCors(event, {})
     })
-  })
-)
+  )
+  .use(
+    '/graphql',
+    eventHandler(async (event) => {
+      const body = await readBody(event)
+
+      return graphql({
+        schema,
+        source: body.query,
+      })
+    })
+  )
 
 const server = createServer(toNodeListener(app))
 
