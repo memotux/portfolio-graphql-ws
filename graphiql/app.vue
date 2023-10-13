089cad71ed5c721a69dc7fe3f2f84125427f6912
memoTux <romeo@mendezfuentes.net>
2023-10-13 16:25:13 -0600

    [graphiql] add GraphiQL component

--- graphiql/app.vue
+++ graphiql/app.vue
@@ -1,5 +1,46 @@
+<script setup>
+useHead({
+  title: 'GraphiQL',
+  script: [
+    {
+      crossorigin: '',
+      async: 'true',
+      src: 'https://unpkg.com/react@18.2.0/umd/react.development.js',
+    },
+    {
+      crossorigin: '',
+      async: 'true',
+      src: 'https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js',
+    },
+    {
+      type: 'application/javascript',
+      async: 'true',
+      src: 'https://unpkg.com/graphiql@3.0.6/graphiql.min.js',
+    },
+  ],
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://unpkg.com/graphiql@3.0.6/graphiql.min.css',
+    },
+  ],
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <UContainer
+    as="main"
+    class="p-8"
+    :ui="{
+      constrained: 'max-w-[100dvw] h-full',
+    }"
+  >
+    <GraphiQL />
+  </UContainer>
 </template>
+
+<style>
+#__nuxt {
+  height: 100dvh;
+}
+</style>
---
+++ graphiql/components/GraphiQL.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { createClient } from 'graphql-ws'
+onMounted(() => {
+  if (window.ReactDOM) {
+    const fetcher = window.GraphiQL.createFetcher({
+      url: 'http://localhost:3000/graphql',
+      wsClient: createClient({
+        url: 'ws://localhost:3000/graphql',
+      }),
+    })
+    const root = window.ReactDOM.createRoot(document.getElementById('graphiql'))
+    root.render(
+      window.React.createElement(window.GraphiQL, {
+        fetcher,
+        defaultEditorToolsVisibility: true,
+      })
+    )
+  }
+})
+</script>
+
+<template>
+  <div
+    id="graphiql"
+    class="h-full w-full"
+  >
+    Loading...
+  </div>
+</template>
