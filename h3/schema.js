772efb5e1ba1c60bb01cd77f86972c8fe517c21b
memoTux <romeo@mendezfuentes.net>
2023-10-13 19:19:54 -0600

    [h3] add Subscription greeting

--- h3/schema.js
+++ h3/schema.js
@@ -4,11 +4,24 @@ const typeDefs = /* GraphQL */ `
   type Query {
     hello: String
   }
+  type Subscription {
+    greeting: String
+  }
 `
 
 const resolvers = {
   Query: {
     hello: () => 'Hello GraphQL',
+  },
+  Subscription: {
+    greeting: {
+      resolve: (payload) => payload,
+      subscribe: async function* () {
+        for (const hi of ['Hi', 'Bonjour', 'Hola', 'Ciao', 'Zdravo']) {
+          yield hi
+        }
+      },
+    },
   },
 }
 
