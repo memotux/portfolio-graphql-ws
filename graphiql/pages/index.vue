e808b9b3b7a61cb9b19b4532085f0aa20aed5a2f
memoTux <romeo@mendezfuentes.net>
2023-10-13 19:20:51 -0600

    [graphiql] add createClient

--- graphiql/pages/index.vue
+++ graphiql/pages/index.vue
@@ -1,3 +1,30 @@
+<script setup>
+import { createClient } from 'graphql-ws'
+import { WebSocket } from 'ws'
+
+const greetings = ref([])
+
+const client = createClient({
+  url: 'ws://localhost:3000/graphql',
+  webSocketImpl: WebSocket,
+})
+
+const subscription = client.iterate({
+  query: 'subscription { greeting }',
+})
+
+onMounted(async () => {
+  for await (const result of subscription) {
+    greetings.value.push(result.data?.greeting)
+    console.log(greetings.value)
+  }
+})
+
+onUnmounted(() => {
+  subscription.return?.()
+})
+</script>
+
 <template>
   <UContainer
     as="main"
