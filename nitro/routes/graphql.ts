48e91392a661e0734a407dd7be5d00531f98cb86
memoTux <romeo@mendezfuentes.net>
2023-10-17 19:12:08 -0600

    [nitro] handle cors

--- nitro/routes/graphql.ts
+++ nitro/routes/graphql.ts
@@ -2,6 +2,7 @@ import { graphql } from 'graphql'
 import schema from '../graphql/schema'
 
 export default eventHandler(async (event) => {
+  handleCors(event, {})
   const body = await readBody(event)
 
   return graphql({
